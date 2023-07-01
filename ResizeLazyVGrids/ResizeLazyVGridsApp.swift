c8a82ea54db1b3b518895724c256a5c8a5d5c88c
githubuiux <uiuxista@gmail.com>
2023-07-01 20:17:49 +0300

    Initial Commit

---
+++ ResizeLazyVGrids/ResizeLazyVGridsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResizeLazyVGridsApp.swift
+//  ResizeLazyVGrids
+//
+//  Created by Levent on 1.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct ResizeLazyVGridsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
